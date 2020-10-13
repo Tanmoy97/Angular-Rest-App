1ac8a4261c1c554f74a7ba2e52f64fc96e23eb78
Tanmoy khatua <53449205+Tanmoy97@users.noreply.github.com>
2020-10-13 17:16:34 +0530

    Add files via upload

--- version.d.ts
+++ version.d.ts
@@ -5,13 +5,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/**
- * @module
- * @description
- * Entry point for all public APIs of the common package.
- */
 import { Version } from '@angular/core';
-/**
- * @stable
- */
+/** Current version of Angular Material. */
 export declare const VERSION: Version;

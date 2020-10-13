b04070832f729d93796b53fd338445de41dd7d22
Tanmoy khatua <53449205+Tanmoy97@users.noreply.github.com>
2020-10-13 16:34:55 +0530

    Add files via upload

--- overlay/index.d.ts
+++ overlay/index.d.ts
@@ -1,8 +1,7 @@
 /**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * Generated bundle index. Do not edit.
  */
-export * from './typings/index';
+export * from './public_api';
+export { OVERLAY_CONTAINER_PROVIDER as ɵb, OVERLAY_CONTAINER_PROVIDER_FACTORY as ɵa } from './overlay-container';
+export { MD_CONNECTED_OVERLAY_SCROLL_STRATEGY as ɵc, MD_CONNECTED_OVERLAY_SCROLL_STRATEGY_PROVIDER as ɵe, MD_CONNECTED_OVERLAY_SCROLL_STRATEGY_PROVIDER_FACTORY as ɵd } from './overlay-directives';
+export { OverlayPositionBuilder as ɵf } from './position/overlay-position-builder';
